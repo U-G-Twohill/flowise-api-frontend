bc58110b1b8029bee09351747c2b98e91177f9c9
Uriah Twohill <ugtwohill@gmail.com>
2023-09-24 13:00:20 +1300

    updataed Flowise API Frontend, Tutorial 8

--- controllers/flowise.js
+++ controllers/flowise.js
@@ -8,7 +8,36 @@ export const createPrediction = async (req, res) => {
       question: message,
     };
 
-    res.status(200).json({ message: "Demo Response" });
+    // Call the Flowise Endpoint here
+    const response = await fetch(
+      `${process.env.FLOWISE_URL}/api/v1/prediction/${process.env.FLOW_ID}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${process.env.FLOWISE_API_KEY}`
+        },
+        body: JSON.stringify(flowiseData),
+      }
+    );
+
+    // Log the raw text response to debug
+    const rawText = await response.text();
+    console.log('Raw text response:', rawText);
+    console.log(`Flowise ID: ${process.env.FLOWISE_ID}`);
+    console.log(`Flowise API Key: ${process.env.FLOWISE_API_KEY}`);
+
+    // Check if the response was successful
+    if (response.ok) {
+      const data = JSON.parse(rawText);
+      console.log(data);
+      res.status(200).json({ message: data });
+    } else {
+      console.log('Error in fetch:', rawText);
+      // Handle error response here
+      res.status(response.status).json({ message: rawText });
+    }
+
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
